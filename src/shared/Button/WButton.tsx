f364698e0188c88d14409e649763dffd741f3901
Bragin Stepan <braginstepan@gmail.com>
2025-02-25 16:06:31 +0500

    фикс кнопки

--- src/shared/Button/WButton.tsx
+++ src/shared/Button/WButton.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
-type Props = {
-  props: React.ButtonHTMLAttributes<HTMLButtonElement>;
+interface WButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  disabled?: boolean;
-};
+}
 
-/** обертка для кнопки */
-export const WButton = ({ children, disabled = false, ...props }: Props) => {
+/** Обертка для кнопки */
+export const WButton: React.FC<WButtonProps> = ({
+  children,
+  className,
+  disabled,
+  ...rest
+}) => {
   return (
     <button
       className={`px-4 py-2 text-white rounded ${
         disabled
-          ? 'bg-gray-300'
+          ? 'bg-gray-300 cursor-not-allowed'
           : 'bg-blue-400 hover:bg-blue-500 hover:cursor-pointer'
-      }`}
-      {...props}
+      } ${className || ''}`}
+      disabled={disabled}
+      {...rest}
     >
       {children}
     </button>
